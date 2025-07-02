577b88ebd61886e2e240eca6e6bc67a477ee7169
Santiago Nuñez Ingas <snunez@chattigo.com>
2025-07-02 15:28:56 -0300

    First commit

---
+++ src/main/java/com/santinuin/musicstore/music_store_api_rest/MusicStoreApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.santinuin.musicstore.music_store_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santinuin/musicstore/music_store_api_rest/MusicStoreApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santinuin.musicstore.music_store_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
